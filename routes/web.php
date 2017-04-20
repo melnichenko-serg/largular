02ffc3a8f950327ffb6fae37bdeb7cfff8268e71
Programel <programel.dev@gmail.com>
2017-04-20 18:09:13 +0300

    add and update back files

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,44 @@
 |
 */
 
+use App\User;
+use App\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::post('/api/auth/signup', function (Request $request) {
+    return User::create($request->all());
+});
+Route::post('/api/auth/login', function (Request $request) {
+    $credentials = ['email' => $request->email, 'password' => $request->password];
+    if (Auth::attempt($credentials)) {
+        $user = User::where('email', $request->email)->first();
+        Auth::login($user);
+        return response()->json($user);
+    } else {
+        die("Fuck off");
+    }
+});
+
+
+Route::get('/api/books', function () {
+    return response()->json(Book::all());
+});
+Route::get('/api/books/{id}', function ($id) {
+    return response()->json(Book::findOrFail($id));
+});
+Route::post('/api/books', function (Request $request) {
+    return Book::create($request->all());
+});
+Route::put('/api/books/{id}', function ($id, Request $request) {
+    $book = Book::findOrFail($id);
+
+    return $book->update($request->all());
+});
+Route::delete('/api/books/{id}', function ($id) {
+    return Book::delete($id);
 });
